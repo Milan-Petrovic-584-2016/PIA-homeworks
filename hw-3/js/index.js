72243fb9deaf65d39e93f5246adc84b574512031
Milan Petrović <milannpetrovic98@gmail.com>
2020-12-24 20:55:56 +0100

    Commit 5
    Dodajem pitanja(to je file data.js), kreirao sam klasu Osoba koja predstavlja igraca i uradio sam jos neke sitne izmene.

--- hw-3/js/index.js
+++ hw-3/js/index.js
@@ -1,14 +1,55 @@
-var slide = 0;
+class Osoba {
+    name = "";
+    boja = "#000000";
+    poeni = 0;
+    pitanje = 0;
+    constructor(ime, boja) {
+        //dopuni za poslednji zadatak
+        this.name = ime;
+        this.boja = boja;
+    }
+    getOsoba() {
+        console.log("ime ", this.name, " poen ", this.poeni, " boja: ", this.boja);
+    }
+
+
+}
+
+
+let slide = 0;
+var timer = 0; //doradi
+var topTen = -1; //niko nije upisan u Top10
+let igrac;
+let ime;
+let boja;
+
+
+
 $(document).ready(function() {
     $("#zi").click(function() {
         $("#pr1").css("display", "none");
         $("#pr2").css("display", "flex");
-        slide++;
+        slide = 1;
+
     })
     $("#sbm").click(function() {
-        $("#pr2").css("display", "none");
-        $("#pr3").css("display", "block");
-        slide++;
+        //dopuni
+        if ($(document.getElementById("inputIme")).val() == "")
+            alert('Unesite ime!!');
+        else if ($(document.getElementById("boja")).val() == "#ffffff") {
+            alert('Uneliste belu boju.Bela se nevidi,unestite drugu!!');
+        } else {
+            ime = document.getElementById("inputIme").value.toString();
+            boja = document.getElementById("boja").value.toString();
+
+            $("#pr2").css("display", "none");
+            $("#pr3").css("display", "block");
+            slide = 2;
+
+
+        }
+
+
     })
     $("#cnl").click(function() {
         $(document.getElementById("inputIme")).val("");
@@ -18,5 +59,11 @@ $(document).ready(function() {
     $("#start").click(function() {
         $("#pr3").css("display", "none");
         $("#pr4").css("display", "block");
+        slide = 3;
+        igrac = new Osoba(ime, boja);
+        //igrac.getOsoba();
+
+
+
     })
 })
