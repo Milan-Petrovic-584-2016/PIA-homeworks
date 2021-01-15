606b7aba4d00d778bc38f0832d7baf674d1706bf
Milan Petrović <milannpetrovic98@gmail.com>
2021-01-15 12:22:29 +0100

    Baza
    Napravio sam bazu podataka

---
+++ hw-4/imdb.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Jan 15, 2021 at 10:21 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `imdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+DROP TABLE IF EXISTS `film`;
+CREATE TABLE IF NOT EXISTS `film` (
+  `id_film` int(11) NOT NULL AUTO_INCREMENT,
+  `naslov` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `opis` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `godina` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slika` varchar(100) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `trajanje` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_ocena` decimal(10,0) NOT NULL,
+  `b_glasova` int(10) NOT NULL,
+  PRIMARY KEY (`id_film`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_glumac`
+--
+
+DROP TABLE IF EXISTS `film_glumac`;
+CREATE TABLE IF NOT EXISTS `film_glumac` (
+  `id_film` int(11) NOT NULL,
+  `id_glumca` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_glumca`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_kuca`
+--
+
+DROP TABLE IF EXISTS `film_kuca`;
+CREATE TABLE IF NOT EXISTS `film_kuca` (
+  `id_film` int(11) NOT NULL,
+  `id_kuce` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_kuce`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_reziser`
+--
+
+DROP TABLE IF EXISTS `film_reziser`;
+CREATE TABLE IF NOT EXISTS `film_reziser` (
+  `id_film` int(11) NOT NULL,
+  `id_reziser` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_reziser`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_scenarista`
+--
+
+DROP TABLE IF EXISTS `film_scenarista`;
+CREATE TABLE IF NOT EXISTS `film_scenarista` (
+  `id_film` int(11) NOT NULL,
+  `id_scenarista` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_scenarista`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_zanr`
+--
+
+DROP TABLE IF EXISTS `film_zanr`;
+CREATE TABLE IF NOT EXISTS `film_zanr` (
+  `id_film` int(11) NOT NULL,
+  `id_zanr` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_zanr`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `glumac`
+--
+
+DROP TABLE IF EXISTS `glumac`;
+CREATE TABLE IF NOT EXISTS `glumac` (
+  `id_glumca` int(11) NOT NULL AUTO_INCREMENT,
+  `ime` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prezime` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_glumca`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `korisnik`
+--
+
+DROP TABLE IF EXISTS `korisnik`;
+CREATE TABLE IF NOT EXISTS `korisnik` (
+  `id_korisnik` int(11) NOT NULL AUTO_INCREMENT,
+  `ime` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prezime` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `u_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sifra` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_korisnik`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kuca`
+--
+
+DROP TABLE IF EXISTS `kuca`;
+CREATE TABLE IF NOT EXISTS `kuca` (
+  `id_kuce` int(11) NOT NULL AUTO_INCREMENT,
+  `naziv` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_kuce`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reziser`
+--
+
+DROP TABLE IF EXISTS `reziser`;
+CREATE TABLE IF NOT EXISTS `reziser` (
+  `id_reziser` int(11) NOT NULL AUTO_INCREMENT,
+  `ime` int(50) NOT NULL,
+  `prezime` int(50) NOT NULL,
+  PRIMARY KEY (`id_reziser`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scenatista`
+--
+
+DROP TABLE IF EXISTS `scenatista`;
+CREATE TABLE IF NOT EXISTS `scenatista` (
+  `id_scenarista` int(11) NOT NULL AUTO_INCREMENT,
+  `ime` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prezime` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_scenarista`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zanr`
+--
+
+DROP TABLE IF EXISTS `zanr`;
+CREATE TABLE IF NOT EXISTS `zanr` (
+  `id_zanr` int(11) NOT NULL AUTO_INCREMENT,
+  `naziv` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_zanr`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
